c90e72138ca01aa1acafadaea9d68f0b1dfde3ce
pooya <pooya_alen1990@yahoo.com>
2015-02-16 12:34:43 -0800

    advertise adapter dare kamel mishe :D

--- app/src/main/java/com/rayweb/brand/AdvertiseAdapter.java
+++ app/src/main/java/com/rayweb/brand/AdvertiseAdapter.java
@@ -16,4 +16,29 @@ public class AdvertiseAdapter extends ArrayAdapter<StructureAdvertise> {
     public AdvertiseAdapter(ArrayList<StructureAdvertise> advertises){
         super(Global.context , R.layout.adapter_advertise , advertises);
     }
+
+    private static class ViewHolder{
+        public ViewHolder(View view){
+
+        }
+        public void fill(ArrayAdapter<StructureAdvertise> adapter,StructureAdvertise item,int position){
+
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+        ViewHolder holder;
+        StructureAdvertise item = getItem(position);
+        if(convertView == null){
+            convertView = Global.inflater.inflate(R.layout.adapter_advertise , parent,false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.fill(this,item,position);
+        return convertView;
+    }
 }
