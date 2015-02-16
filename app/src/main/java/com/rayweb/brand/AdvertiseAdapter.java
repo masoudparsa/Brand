0f1303cc2ef470a34509eee5b557441d9bb11227
masoudparsa <m.parsanejad@gmail.com>
2015-02-16 15:10:38 +0330

    d

--- app/src/main/java/com/rayweb/brand/AdvertiseAdapter.java
+++ app/src/main/java/com/rayweb/brand/AdvertiseAdapter.java
@@ -3,7 +3,6 @@ package com.rayweb.brand;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 
 import com.rayweb.brand.Infrastructure.Global;
 
